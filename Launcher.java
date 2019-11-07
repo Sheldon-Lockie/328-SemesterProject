5945d1330d2b281101a85a82b1716301da81c2ce
Brandon6583 <33532474+Brandon6583@users.noreply.github.com>
2019-11-07 15:01:08 -0800

    Modifying Base classes and fixing other sections of our code

--- Launcher.java
+++ Launcher.java
@@ -2,21 +2,17 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-
-
+// This class create an instance of the game
 public class Launcher
 {
+    private static Game myGame;
     public static void main (String[] args)
     {
-        Game myGame = new MainGame();
+        myGame = new MainGame();
         LwjglApplicationConfiguration cfg =  new LwjglApplicationConfiguration();
         cfg.height = 900;
         cfg.width = 1600;
-        cfg.title = "Final Stand";
-        
-       
-
-        
+        cfg.title = "Final Stand";      
         LwjglApplication launcher = new LwjglApplication (myGame, cfg);
     }
 }
