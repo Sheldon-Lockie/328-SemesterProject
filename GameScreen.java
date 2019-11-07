8c8c87a040a012ae087d3e3065abf170001d2c7d
Sheldon-Lockie <38440279+Sheldon-Lockie@users.noreply.github.com>
2019-11-06 22:05:53 -0800

    Updated GameScene

--- GameScreen.java
+++ GameScreen.java
@@ -13,18 +13,20 @@ public class GameScreen extends BaseScreen
     BaseActor UnitArea;
     public void initialize()
     {
-        HelpButton = new Button(650,550,uiStage);
-        HelpButton.loadTexture("Assets/Img/Buttons/Help_Unhighlighted.png");
-        HelpButton.setSize(32,32);
-        GameArea = new BaseActor(0,100,mainStage);
+      
+        GameArea = new BaseActor(0,161,mainStage);
         GameArea.loadTexture("Assets/Img/PlaceHolders/GameAreaPlaceHolder.png");
         UnitInfoArea =  new BaseActor(0,0,mainStage);
         UnitInfoArea.loadTexture("Assets/Img/PlaceHolders/UnitInfoPlaceHolder.png");
-        UnitArea = new BaseActor(700,100,mainStage);
+        UnitArea = new BaseActor(1350,0,mainStage);
         UnitArea.loadTexture("Assets/Img/PlaceHolders/UnitsPlaceHolder.png");
+        HelpButton = new Button(650,550,uiStage);
+        HelpButton.loadTexture("Assets/Img/Buttons/Help_Unhighlighted.png");
+        HelpButton.setSize(32,32);
         Pixmap pm = new Pixmap(Gdx.files.internal("Assets/Img/Cursors/CursorSword.png"));
         Gdx.graphics.setCursor(Gdx.graphics.newCursor(pm, 0, 0));
         pm.dispose();
+        
     }
  
     public void update (float dt)
