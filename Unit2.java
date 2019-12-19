3816af29575fdc7fea807336834c28e779cf5c0b
Sheldon-Lockie <38440279+Sheldon-Lockie@users.noreply.github.com>
2019-12-18 23:17:08 -0800

    Edited Buy Area

--- Unit2.java
+++ Unit2.java
@@ -7,12 +7,19 @@ public class Unit2 extends BaseActor
     public Unit2 (float x, float y, Stage s)
     {
         super(x,y,s);
-        loadTexture("Assets/Img/Towers/Unit2.png");
-        centerAtPosition(GameScreen.MouseX,GameScreen.MouseY);
-        setSize(80,80);
+        loadTexture("Assets/Img/Towers/MageTowerNew.png");
+        //centerAtPosition(GameScreen.MouseX,GameScreen.MouseY);
+        float sizeX = 70;
+        float sizeY = 120;        
+        setSize(sizeX, sizeY);
+        centerAtPosition2(x, y, sizeX, sizeY);
         setBoundaryPolygon(4);
         
     }
-    
+     public void act(float dt)
+    {
+        super.act(dt);
+        this.toFront();
+    }
     
 }
